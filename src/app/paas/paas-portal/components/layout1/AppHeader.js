d4755817f4f0c19fdb1f29fdade921fcb63ec774
qiraosky <qiraosky@qq.com>
2018-06-22 11:46:16 +0800

    增加下拉框

--- src/app/paas/paas-portal/components/layout1/AppHeader.js
+++ src/app/paas/paas-portal/components/layout1/AppHeader.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Menu , Avatar , Badge } from 'antd';
+import { Layout, Menu , Avatar , Badge , Popover , Icon} from 'antd';
 import { connect } from 'react-redux';
 const { Header } = Layout;
 import { topMenuSwitchAction  } from '../../actions/Actions';
@@ -23,15 +23,36 @@ class AppHeader extends React.Component {
     constructor(props){
         super(props)
     }
+    messageContent = (
+        <div style={{textAlign:"left"}}>
+          <p>您有一条待办流程</p>
+          <p>流程《ABC》已办结</p>
+        </div>
+      );
+    accountContent = (
+        <div style={{width:"130px"}}>
+          <p style={{cursor: "pointer"}}><Icon type="user" /> <span style={{marginLeft:"10px"}}>个人中心</span> </p>
+          <p style={{cursor: "pointer"}}><Icon type="setting" /> <span style={{marginLeft:"10px"}}>设置</span></p>
+          <p style={{cursor: "pointer"}}><Icon type="logout" /> <span style={{marginLeft:"10px"}}>退出登陆</span> </p>
+        </div>
+    )
+
     render(){
         return(
             <Header className="header" style={{paddingLeft:"0px"}}>
-            <div className="logo" > LOGO DIV</div>
-            <div style={{width:"100px",height:"64px",color:"#FFF",float:"right"}}>
-                <Badge style={{marginTop:"20px",zIndex:"10000",marginLeft:"-20px"}} count={1}>
-                    <Avatar size="large" style={{marginTop:"12px",background:"#001529",marginRight:"10px",cursor: "pointer"}} icon="bell" />
-                </Badge>
-                <Avatar size="large" icon="user" style={{marginTop:"12px",background:"#001529", cursor: "pointer"}}/>
+            <div className="logo" > CLOUD STAGE LOGO</div>
+            <div style={{width:"100px",height:"64px",color:"#FFF",float:"right",cursor: "pointer"}}>
+
+                <Popover content={this.messageContent} placement="bottomRight">
+                    <Badge style={{marginTop:"20px",zIndex:"10000",marginLeft:"-20px"}} 
+                        count={1000}>
+                        <Avatar size="large" style={{marginTop:"12px",background:"#001529",marginRight:"10px",cursor: "pointer"}} icon="bell" />
+                    </Badge>
+                </Popover>
+
+                <Popover content={this.accountContent} placement="bottomRight">
+                    <Avatar size="large" icon="user" style={{marginTop:"12px",background:"#001529", cursor: "pointer"}}/>
+                </Popover>
 
             </div>
             <Menu
