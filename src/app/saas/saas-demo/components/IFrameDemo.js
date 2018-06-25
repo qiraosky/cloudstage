b43fa92f582602d7357a8832adf4f5567fb1d2f1
qiraosky <qiraosky@qq.com>
2018-06-25 21:20:36 +0800

    save

--- src/app/saas/saas-demo/components/IFrameDemo.js
+++ src/app/saas/saas-demo/components/IFrameDemo.js
@@ -3,7 +3,7 @@ import React from 'react'
 class IFrameDemo extends React.Component {
     render() {
         return (
-            <div><iframe style={{width:'100%',height:'100%'}} src="http://www.baidu.com"></iframe></div>
+            <div><iframe style={{width:'100%',height:'1000px',border:"0px"}} src="http://www.baidu.com"></iframe></div>
         )
     }
 }
