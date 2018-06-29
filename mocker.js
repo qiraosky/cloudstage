4fd0fcdee07aff78065a6ce1bc6bfed24c38f800
qiraosky <qiraosky@qq.com>
2018-06-29 18:28:36 +0800

    将mock转为连接mongo方式

--- mocker.js
+++ mocker.js
@@ -3,27 +3,48 @@ const legcy = require('./mocker/legcy');
 const paasPortal = require('./mocker/paas-portal');
 const paasIam = require('./mocker/paas-iam');
 const saasDemo = require('./mocker/saas-demo');
+const uuid = require('node-uuid');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://appuser:111111@140.143.187.43:27017/gcable";
+const version = '0.1';
+/* 
+//数据生成代码，请使用 node 运行
+//依赖库：
+//node install mongodb
+//node install node-uuid
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://appuser:111111@140.143.187.43:27017/gcable";
 const uuid = require('node-uuid');
 
-const projectList = [];
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("gcable");
 
-for(let i=1;i<=60;i++){
-    let bean = {
-        projectId:uuid.v4(),
-        projectCode:`AWVBD${i<10?"0"+i:i}`,
-        name:`项目${i}`,
-        status:i%3==0?'end':i%2==0?'pending':'start',
-        starttime:new Date(),
-        endtime:new Date(),
-        location:i%3==0?'北京':i%2==0?'上海':'广州',
-        principal:i%3==0?'杰克':i%2==0?'约翰':'彼得',
-    }
-    projectList.push(
-        bean
-    )
-    console.log(JSON.stringify(bean))
-}
+  	const projectList = [];
+	for(let i=1;i<=60;i++){
+	    let bean = {
+	        projectId:uuid.v4(),
+	        projectCode:`AWVBD${i<10?"0"+i:i}`,
+	        name:`项目${i}`,
+	        status:i%3==0?'end':i%2==0?'pending':'start',
+	        starttime:new Date(),
+	        endtime:new Date(),
+	        location:i%3==0?'北京':i%2==0?'上海':'广州',
+	        principal:i%3==0?'杰克':i%2==0?'约翰':'彼得',
+	    }
+	    projectList.push(
+	        bean
+	    )
+	    console.log(JSON.stringify(bean))
+	}
+
+  dbo.collection("project").insertMany(projectList, function(err, res) {
+      if (err) throw err;
+      console.log("文档插入成功");
+      db.close()
+  });
+}); */
 
 
 //在此页面的接口调整保存即生效
@@ -31,16 +52,15 @@ const fastApi = {
     'GET /demo/test': (req, res) => {
         res.send({ status: 'ok', message: '测试成功 ！' });
     },
-    'POST /demo/listProject':(req, res) => {
+    'POST /demo/listProject': (req, res) => {
         let param = req.body;
-        console.log(param)
         let returnVal = {
             info: {
                 "seed": "0e03f93fd1dad64e", 
                 "pageSize": 10, 
-                "total":projectList.length,
+                "total":0, 
                 "page": 1, 
-                "version": "1.2"
+                "version": version
             },
             results:[]
         };
@@ -54,55 +74,95 @@ const fastApi = {
             param.page = param.page -1;
         }
         let start = param.pageSize * param.page ;
-        for(let i = start  ;i< start + param.pageSize; i++){
-               if(projectList[i])
-                    returnVal.results.push(projectList[i]);
-        }
 
-        res.send(returnVal);
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("gcable");
+             var whereStr = {};  // 查询条件
+            dbo.collection("project").find(whereStr).skip(start).limit(param.pageSize).toArray(function(err, result) {
+                if (err) throw err;
+                db.close();
+                returnVal.results = result;
+                MongoClient.connect(url, function(err, db) {
+                    if (err) throw err;
+                    var dbo = db.db("gcable");
+                     var whereStr = {};  // 查询条件
+                    dbo.collection("project").count(whereStr,function(err, result) {
+                        if (err) throw err;
+                        db.close();
+                        console.log(result)
+                        returnVal.info.total = result;
+                        res.send(returnVal);
+                    });
+                });
+                
+            });
+        });
+
+        
+
+
+        //res.send({ status: 'ok', message: '测试成功 ！' });
     },
     'POST /demo/deleteProject':(req, res) => {
         let param = req.body;
-        let deleteCount = 0;
-        for(let i=0;i<projectList.length;i++){
-            if(param.projectId == projectList[i].projectId){
-                let bean = projectList.splice(i,1);
-                deleteCount ++ ;
-                console.log(bean)
-                break;
-            } 
-        }
-        res.send({ status: 'ok', message: '删除成功 ！' ,deleteCount: deleteCount}); 
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("gcable");
+            var whereStr = {projectId:param.projectId};  // 查询条件
+            console.log(whereStr)
+            dbo.collection("project").remove(whereStr,function(err, result) {
+                if (err) throw err;
+                db.close();
+                console.log(result)
+                res.send({ status: 'ok', message: '删除成功 ！' ,deleteCount: result.result.n});
+            });
+        });
     },
     'POST /demo/getProject':(req, res) => {
         let param = req.body;
-        for(let i=0;i<projectList.length;i++){
-            if(param.projectId == projectList[i].projectId){
-                let bean = projectList[i];
-                res.send({ status: 'ok', project: bean}); 
-                return;
-            }
-        }
-        res.send({ status: 'not found', project: {}}); 
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("gcable");
+            var whereStr = {projectId:param.projectId};  // 查询条件
+            console.log(whereStr)
+            dbo.collection("project").find(whereStr).limit(1).toArray(function(err, result) {
+                if (err) throw err;
+                db.close();
+                let bean = {};
+                if(result && result instanceof Array && result.length > 0){
+                    bean = result[0]
+                }
+                res.send({ status: 'ok', project: bean});
+            });
+        });
     },
     'POST /demo/saveProject':(req, res) => {
-        console.log("saveData processing")
         let param = req.body;
         param.projectId = uuid.v4()
-        projectList.unshift(param)
-        res.send({ status: 'ok', project: param}); 
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("gcable");
+          
+            dbo.collection("project").insertOne(param, function(err, result) {
+                if (err) throw err;
+                db.close()
+                res.send({ status: 'ok', project: param});
+            });
+        });
     },
     'POST /demo/updateProject':(req, res) => {
-        console.log("updateData processing")
         let param = req.body;
-        for(let i=0;i<projectList.length;i++){
-            if(param.projectId == projectList[i].projectId){
-                projectList[i] = param;
-                res.send({ status: 'ok', project: projectList[i]}); 
-                return;
-            }
-        }
-        res.send({ status: 'not found', project: {}}); 
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            let dbo = db.db("gcable");
+            let whereData = {projectId:param.projectId}; 
+            dbo.collection("project").update(whereData, param , function(err, result) {
+                if (err) throw err;
+                db.close()
+                res.send({ status: 'ok', project: param});
+            });
+        });
     }
 }
  
